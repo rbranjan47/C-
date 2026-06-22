d948f9774f4d304fedfee08adce67f669111b4cf
Rabi Ranjan Kumar <rbranjan47@gmail.com>
2026-06-22 23:57:26 +0530

    functions

--- C++ New/functions_cpp.cpp
+++ C++ New/functions_cpp.cpp
@@ -24,14 +24,37 @@ int multiply(int a, int b) {
     return a * b;
 }
 
-// Dividion function
+// Division function
 int division(int a, int b) {
     return a / b;
 }
 
+void swap_two_numbers_call_by_value(int a, int b) {
+    cout << "Initial numbers, a= " << a << " and b= " << b << endl;
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After swapping,  numbers, a= " << a << " and b= " << b << endl;
+}
+
+void check_odd_even_number_call_by_reference(int *a) {
+    // Add * before the parameter use
+    if (*a % 2 == 0) {
+        cout << *a << " is even number" << endl;
+    } else {
+        cout << *a << " is odd number" << endl;
+    }
+}
+
 int main() {
-    cout << add(10, 20) << endl;
-    cout << subtract(10, 20) << endl;
-    cout << multiply(10, 20) << endl;
-    cout << division(10, 20) << endl;
+    int a = 10, b = 20;
+    cout << add(a, b) << endl;
+    cout << subtract(a, b) << endl;
+    cout << multiply(a, b) << endl;
+    cout << division(a, b) << endl;
+    swap_two_numbers_call_by_value(a, b);
+
+    check_odd_even_number_call_by_reference(&a);
 }
